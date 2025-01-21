6cb43c08e142efe853c52691ad415cff62da12ab
harrison curtis <harrisoncurtis50@gmail.com>
2025-01-21 11:41:37 +0000

    Add psoterior drawe mean code and factor out the non-essential code from this script into the other commited scripts

--- bayes_cuped.R
+++ bayes_cuped.R
@@ -1,38 +1,15 @@
-# Load project code.
+# Load project code. ----
 source("utils.R")
-# Load libraries
-library(ggplot2)
+
+# Libraries ----
 library(patchwork)
 library(cmdstanr)
 
 options(mc.cores = parallel::detectCores())
 
 d <- generate_data()
-
-# Data vis
-p1 <- ggplot(d, aes(revenue0, fill = as.factor(ad_campaign))) +
-  geom_density(alpha = 0.5) + labs(title = "Revenue Pre Campaign",
-                                   fill = "Ad Campaign", x = "") + theme(plot.title = element_text(hjust = 0.5))
-
-p2 <- ggplot(d, aes(revenue1, fill = as.factor(ad_campaign))) +
-  geom_density(alpha = 0.5) + labs(title = "Revenue Post Campaign",
-                                   fill = "Ad Campaign", x = "Revenue") +
-                                   theme(plot.title = element_text(hjust = 0.5))
-p1/p2
-
-# Statistical calculations.
-diff_mean <- mean(d[d$ad_campaign==1,"revenue1" ]) - mean(d[d$ad_campaign==0,"revenue1" ])  
-diff_mean
-
-# The same can be achieved using ols lnear model.
-# Model matrix
-ols_fit <- lm(revenue1 ~ ad_campaign ,data = d)
-summary(ols_fit)
-
-dm = model.matrix(ols_fit)
-
-# Bayesian section
-
+dm <- model.matrix(revenue1 ~ ad_campaign, data = d) 
+  
 # Compile the Stan models. 
 mod_lm <- cmdstan_model(stan_file = "stan/lm.stan")
 mod_cuped <- cmdstan_model(stan_file = "stan/cuped.stan")
@@ -54,7 +31,8 @@ bayescuped_fit <- mod_cuped$sample(
 
 print(bayescuped_fit$summary())
 
-theta_mean = theta_mean
+theta_mean = round(mean(bayescuped_fit$draws("theta")), 1)
+
 
 data_sensitivity_list <- list(N = nrow(d), treatment = d$ad_campaign,
                               y_pre = d$revenue0, y_post = d$revenue1, theta = theta_mean)
