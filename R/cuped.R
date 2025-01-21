f745fa46a216c8c0e93dcdbf490bf1ecf9cb7595
harrison curtis <harrisoncurtis50@gmail.com>
2025-01-21 16:32:30 +0000

    Add cuped estiamtor code

--- R/cuped.R
+++ R/cuped.R
@@ -9,4 +9,19 @@ diff_mean
 
 # The same can be achieved using ols linear model.
 ols_fit <- lm(revenue1 ~ ad_campaign, data = d)
-summary(ols_fit)
+ols_summ <-summary(ols_fit)
+
+#CUPED estimator
+theta <- lm(revenue1 ~ revenue0, data = d)
+theta <- theta$coefficients[2]
+
+y_cuped <- d$revenue1 - theta * d$revenue0 - mean(d$revenue0)
+d$y_cuped <- y_cuped
+
+cuped_fit <- lm(y_cuped ~ ad_campaign, data = d) 
+cuped_summ <- summary(cuped_fit)
+
+# Calculate percentage difference of std error of estimators
+ols_ste <- ols_summ$coefficients[4]
+cuped_ste <- cuped_summ$coefficients[4]
+percent_ste_diff <-percent_diff(high_value = ols_ste, low_value = cuped_ste)
