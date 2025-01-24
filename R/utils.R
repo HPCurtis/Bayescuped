7d046d52578b6793e530816a44d0f37a30feb8e4
harrison curtis <harrisoncurtis50@gmail.com>
2025-01-24 13:40:03 +0000

    first SBC attempt does not work

--- R/utils.R
+++ R/utils.R
@@ -22,31 +22,6 @@ generate_data <- function(alpha = 5, beta = 0, gamma = 3, delta = 2, n = 100,
   
   return(data)
 }
-
-generate_priors <- function(n, seed = 42){
-  
-  library(fdrtool)
-  set.seed(seed)
-  
-  # Individuals
-  individual <- 1:n
-  
-  # Treatment status
-  d <- rbinom(n, 1, 0.5)
-  
-  # Priors
-  alpha_target <- rnorm(n=1, mean = 0, sd = 2)
-  theta <- rnorm(n=1, mean = 0, sd = 2)
-  sigma_theta <- rhcauchy(1, sigma = 1)
-
-  alpha_cuped <- rnorm(1, 0, 2)
-  beta_cuped <- rnorm(1, 0, 3)
-  sigma_cuped <- rhcauchy(1, sigma = 1)
-
-  # Transforms
-  mu_theta <- alpha_target + theta * y_pre
-
-}
   
 summary_print<- function(sm){
   print(sm$summary())
@@ -55,7 +30,47 @@ summary_print<- function(sm){
 percent_diff <- function(high_value, low_value) {
   ((high_value - low_value) / high_value) * 100
 }
+N <- 100
+sbc_data_generator <- function(N) {
 
-cuped_generator <- function(N){
+  # Generate data based on the prior distributions
+  intercept_target <- rnorm(1, mean = 0, sd = 2)
+  theta <- rnorm(1, mean = 0, sd = 3)
+  sigma_theta <- abs(rcauchy(1, location = 0, scale = 2))
   
+  intercept_cuped <- rnorm(1, mean = 0, sd = 2)
+  beta_cuped <- rnorm(1, mean = 0, sd = 3)
+  sigma_cuped <- abs(rcauchy(1, location = 0, scale = 2))
+  
+  # Generate predictors
+  treatment <- rbinom(N, 1, 0.5)
+  y_pre <- rnorm(N, mean = 0, sd = 1)
+  
+  # Transformed data
+  m_ypre <- mean(y_pre)
+  
+  # Simulate y_post and y_cuped based on the model
+  mu_theta <- intercept_target + theta * y_pre
+  y_post <- rnorm(N, mean = mu_theta, sd = sigma_theta)
+  
+  y_cuped <- y_post - theta * (y_pre - m_ypre)
+  
+  # Return the generated data
+  list(
+    variables = list(
+      intercept_target = intercept_target,
+      theta = theta,
+      sigma_theta = sigma_theta,
+      intercept_cuped = intercept_cuped,
+      beta_cuped = beta_cuped,
+      sigma_cuped = sigma_cuped
+    ),
+    generated = list(
+    N = N,
+    treatment = treatment,
+    y_pre = y_pre,
+    y_post = y_post,
+    m_ypre = m_ypre
+    )
+  )
 }
